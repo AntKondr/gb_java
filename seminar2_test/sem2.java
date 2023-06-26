80962ef20b3f4fa79b1dcbd1157cad486c600a68
Anton_Kondratev <elkondro@gmail.com>
2023-06-26 12:26:16 +0700

    sem2 test done

---
+++ seminar2_test/sem2.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.SimpleFormatter;
+
+public class sem2 {
+    private static Scanner scaner = new Scanner(System.in);
+    private static Logger logger = Logger.getLogger(sem2.class.getName());
+    private static SimpleFormatter simple_formatter = new SimpleFormatter();
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+
+    // 1) Реализуйте алгоритм сортировки пузырьком числового массива,
+    // результат после каждой итерации запишите в лог-файл.
+    public static void task1() {
+        int[] arr = {5, 2, 6, 8, 3, 4, 1, 6, 0};
+
+        logger.info(Arrays.toString(arr) + " исходное состояние\n");
+        // сортировка пузырьком
+        int tmp;
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int i = 0; i < arr.length - 1 - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                }
+            }
+            logger.info(Arrays.toString(arr) + " итерация " + j + "\n");
+        }
+    }
+
+
+
+
+    // 2) К калькулятору из предыдущего ДЗ добавить логирование.
+    public static void task2() {
+        StringBuilder strbld = new StringBuilder();
+
+        System.out.print("Введите первое число: ");
+        String str_a = scaner.nextLine();
+        double a = Double.parseDouble(str_a);
+
+        System.out.print("Введите оператор: ");
+        String op = scaner.nextLine();
+
+        System.out.print("Введите второе число: ");
+        String str_b = scaner.nextLine();
+        double b = Double.parseDouble(str_b);
+
+        double res = 0;
+
+        strbld.append(str_a)
+              .append(op)
+              .append(str_b)
+              .append("=");
+
+        if (op.equals("+")) {
+            res = a + b;
+            strbld.append(res);
+            logger.info(strbld.toString());
+        }
+        else if (op.equals("-")) {
+            res = a - b;
+            strbld.append(res);
+            logger.info(strbld.toString());
+        }
+        else if (op.equals("*")) {
+            res = a * b;
+            strbld.append(res);
+            logger.info(strbld.toString());
+        }
+        else if (op.equals("/")) {
+            res = a / b;
+            strbld.append(res);
+            logger.info(strbld.toString());
+        }
+        else {
+            logger.log(Level.WARNING, "unsupported operator " + op);
+        }
+        strbld.delete(0, strbld.length());
+    }
+}
