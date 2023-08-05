082b0e69ccb5b6e588be41a722bf33e015747215
Anton_Kondratev <elkondro@gmail.com>
2023-08-05 11:31:09 +0700

    Task 2 solution

--- exceptions_2/Main.java
+++ exceptions_2/Main.java
@@ -13,6 +13,8 @@ public class Main {
         }
     }
 
+
+    // задание 1:
     public static float task1() {
         // Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
         // и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
@@ -34,6 +36,8 @@ public class Main {
         return fl;
     }
 
+
+    // задание 4:
     public static String task4() throws Exception {
         // Разработайте программу, которая выбросит Exception,
         // когда пользователь вводит пустую строку.
@@ -44,5 +48,49 @@ public class Main {
             throw new Exception("Пустые строки вводить нельзя");
         }
         return input;
+    }
+
+
+    // Задание 2:
+    // Поместил код в метод, иначе он как-то бессмысленно выглядит.
+    public static void task2(int[] intArray, int index, int d) {
+        try {
+            double catchedRes1 = intArray[index] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        }
+        catch (ArithmeticException | IndexOutOfBoundsException | NullPointerException e) {
+            System.out.println("Catching exception: " + e.getMessage());
+        }
+    }
+}
+
+
+// Задание 3:
+class task3 {
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Dividing by zero");
+        }
+        catch (NullPointerException e) {
+            System.out.println("Exception: a or b is null");
+        }
+        catch (IndexOutOfBoundsException ex) {
+            System.out.println("Индекс за пределами массива!");
+        }
+        catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws NullPointerException {
+        System.out.println(a + b);
     }
 }
